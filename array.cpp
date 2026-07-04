5405cd84e4514a175d711332f1fd69077e2a9c35
manumanju1 <manoj.manjunath1000@gmail.com>
2026-07-04 19:52:43 +0530

    reverse array

--- array.cpp
+++ array.cpp
@@ -1,4 +1,4 @@
-//largest element in an array
+/*//largest element in an array
 #include<iostream>
 using namespace std;
 int main()
@@ -16,4 +16,53 @@ int main()
     cout<<"maximum element :"<<max<<endl;
 
     
+}*/
+
+/*//second largest element
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={5,6,3,7,22,10};
+    int first=0;
+    int second=0;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>first)
+        {
+            
+            second=first;
+            first=arr[i];
+        }
+        else if(arr[i]>second&& arr[i]!=first)
+        {
+            second=arr[i];
+        }
+
+    }
+   cout<<second;
+}*/
+
+//reverse an arry
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int arr[]={5,6,3,7,22,10};
+    
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int start=0;
+    int end =n-1;
+    while(start<end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
